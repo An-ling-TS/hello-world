6759ab0caea1dcbe8c64f7b5e96d39954a2bb4da
lianganqing <32219252+lianganqing@users.noreply.github.com>
2017-10-10 21:38:57 +0800

    Add files via upload

---
+++ homework10.10.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int x=2.2;
+	printf("x=%d\n",x);
+	short y=0x7fff;
+	printf("y=%f\n",y);
+	short z=0xffff;
+	printf("z=%f\n",z);
+	unsigned short w=0xffff;
+	printf("w=%f\n",w);
+	float u=0.177;
+	printf("u=%f\n",u);
+}
