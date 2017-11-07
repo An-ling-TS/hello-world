efff8ebe058e96aaf0c82020048bf21748c2ec8a
Rain <32219252+lianganqing@users.noreply.github.com>
2017-11-07 09:22:35 +0800

    Add files via upload

---
+++ 查询闰年.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("输入年份\n");
+	scanf("%d",&year);
+	
+
+	if(year%100==0 && year%400==0)
+	{
+		printf("%d是闰年"，year); 
+	}
+	else ifyear(uear%4==0)
+	{
+		printf("%d是闰年"，year); 
+	 } 
+	 else
+	 {
+	 	printf
+	 }
+	
+	return 0;
+ } 
